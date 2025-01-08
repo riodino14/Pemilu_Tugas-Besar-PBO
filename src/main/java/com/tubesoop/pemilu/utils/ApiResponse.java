e444f316300c1b4a12b5823bbfe1ab61fdb7e50e
Puguhariyaa <147800667+Puguhariyaa@users.noreply.github.com>
2025-01-08 23:27:38 +0700

    Update ApiResponse.java

--- src/main/java/com/tubesoop/pemilu/utils/ApiResponse.java
+++ src/main/java/com/tubesoop/pemilu/utils/ApiResponse.java
@@ -2,27 +2,6 @@ package com.tubesoop.pemilu.utils;
 
 
 public class ApiResponse {
-    private String status; // success or error
-    private String message; // Informasi tambahan
-
-    public ApiResponse(String status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+    private
     }
 }
