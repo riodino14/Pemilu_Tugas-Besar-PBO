63c74631fd14fc9f8798ae991f0bb4852d31d2fd
sha_azzam <63727742+azzam20-max@users.noreply.github.com>
2025-01-09 12:16:24 +0700

    Update PemilihRepository.java
    add comentar

--- src/main/java/com/tubesoop/pemilu/repository/PemilihRepository.java
+++ src/main/java/com/tubesoop/pemilu/repository/PemilihRepository.java
@@ -1,15 +1,22 @@
 package com.tubesoop.pemilu.repository;
 
-
+// JpaRepository menyediakan berbagai metode bawaan untuk mengelola operasi database.
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.tubesoop.pemilu.entity.Pemilih;
 
 
+// Tipe generik pertama, Pemilih, adalah entity yang akan dimanipulasi.
+// Tipe generik kedua, String, adalah tipe data primary key dari entity Pemilih.
 public interface PemilihRepository extends JpaRepository<Pemilih, String> {
+  
+    // Metode ini mengembalikan objek Pemilih yang sesuai dengan "nik" yang diberikan, atau null jika tidak ditemukan.
     Pemilih findByNik(String nik);
-    Pemilih findByNoTelp(String noTelp);
-    Pemilih findByEmail(String email);
-    
-}
 
+    // Metode ini juga mengembalikan objek Pemilih yang sesuai dengan nomor telepon yang diberikan.
+    Pemilih findByNoTelp(String noTelp);
+
+    // Sama seperti metode sebelumnya, ini mengembalikan objek Pemilih yang cocok dengan email yang diberikan.
+    Pemilih findByEmail(String email);
+}
