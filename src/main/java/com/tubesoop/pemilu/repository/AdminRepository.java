c89334d6552cd31b3a53c561651a68e2fd4ff72b
sha_azzam <63727742+azzam20-max@users.noreply.github.com>
2025-01-09 12:35:35 +0700

    Update AdminRepository.java
    add comment

--- src/main/java/com/tubesoop/pemilu/repository/AdminRepository.java
+++ src/main/java/com/tubesoop/pemilu/repository/AdminRepository.java
@@ -1,10 +1,16 @@
 package com.tubesoop.pemilu.repository;
 
-
-
+// Entity Admin merepresentasikan tabel dalam database.
 import com.tubesoop.pemilu.entity.Admin;
+
+// Mengimpor JpaRepository dari Spring Data JPA.
+// JpaRepository menyediakan berbagai operasi database bawaan seperti save, delete, dan find.
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+// Admin adalah nama entity, dan Long adalah tipe data primary key dari entity tersebut.
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    // findByEmail menggunakan kolom "email" dalam tabel Admin sebagai parameter pencarian.
     Admin findByEmail(String email);
 }
